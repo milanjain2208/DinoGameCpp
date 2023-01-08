a8d97b05b2fc3293b79fa083477d4be25c2dae1f
milanjain2208 <jameschabbra22@gmail.com>
2023-01-08 18:03:29 +0000

    Implementation of Dinosaur class: incomplete

---
+++ build/CMakeFiles/DinoGame.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DinoGame.dir/src/main.cpp.o"
+  "CMakeFiles/DinoGame.dir/src/renderer.cpp.o"
+  "CMakeFiles/DinoGame.dir/src/game.cpp.o"
+  "CMakeFiles/DinoGame.dir/src/controller.cpp.o"
+  "CMakeFiles/DinoGame.dir/src/dinosaur.cpp.o"
+  "DinoGame.pdb"
+  "DinoGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DinoGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
